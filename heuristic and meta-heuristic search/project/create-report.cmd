956643df830d2130dadec3c4142837743bd5f22d
Diego Lovison <diegolovison@gmail.com>
2014-06-15 18:58:17 -0300

    report

--- heuristic and meta-heuristic search/project/create-report.cmd
+++ heuristic and meta-heuristic search/project/create-report.cmd
@@ -1,2 +1,14 @@
-run.cmd 20 5 > report\20-5.log
-run.cmd 20 10 > report\20-10.log
+call run.cmd 20 5 > report\20-5.log
+call run.cmd 20 10 > report\20-10.log
+call run.cmd 20 20 > report\20-20.log
+
+call run.cmd 50 5 > report\50-5.log
+call run.cmd 50 10 > report\50-10.log
+call run.cmd 50 20 > report\50-20.log
+
+call run.cmd 100 5 > report\100-5.log
+call run.cmd 100 10 > report\100-10.log
+call run.cmd 100 20 > report\100-20.log
+
+call run.cmd 200 10 > report\200-10.log
+call run.cmd 200 20 > report\200-20.log
